5c25d19fc3b445ec34a30cfa23bd4d44bd053d55
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-19 18:02:10 -0700

    added bugs found and imported reacticons as needed for use in components

--- SimpleTodoList/src/Components/Todo/Todo.tsx
+++ SimpleTodoList/src/Components/Todo/Todo.tsx
@@ -1,5 +1,7 @@
 // imports typically live here
 import { useState } from 'react'
+import { FaRegEdit } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 import styles from './Todo.module.css'
 
 // our interface with props we will use go here
@@ -16,14 +18,48 @@ const Todo = () => {
     // useState for tracking our list
     const [list, setList] = useState<TodoItem[]>([]);
 
-    // function to add items to our list
+    // function to add items to our list and clear the input field after function is run
     const addToDo = (newItem:string) => {
-        
+
+        // create an object that will add new items to our list
+        const newTodo:TodoItem = {
+            id: Math.floor(Math.random()*1000),
+            taskName: newItem,
+            done: false
+        }
+        setList([...list,newTodo]);
+        setInput("");
+        console.log(`New item ID is ${newTodo.id}`)
     }
+
+    // create a delete function and connect it to the X on the delete button in the li of our organized list
+
+    // create an edit/update function and connect it to the pencil icon which sits to the left of the X delete button
+    
 
   return (
     <>
+        {/* This will populate our page with our List Title, input field, buttons and display each todo items */}
+        <div className='listContainer'>
+            <div className="row">
+                <h1>Dad's List</h1>
+                <input type='text' value={input} onChange={(e)=>setInput(e.target.value)}/>
+                <button className='addButton' onClick={()=>addToDo(input)}>Add</button>
+            </div>
 
+            {/* render list items within an ol inside of li tags that will be mapped with our list array */}
+            {/* I want to create onMouseOver to change the icon to its negative <FaEdit /> HOW TO DO? */}
+            <ol>
+                {list.map(item => (
+                    <li key={item.id}>
+                        {item.taskName}
+                        <button className='editButton'><FaRegEdit /></button>
+                        <button>X</button>
+
+                    </li>
+                ))}
+            </ol>
+        </div>
     </>
   )
 }
